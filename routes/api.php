cb572bdae45082d07811dd7e6414b1e0202634fb
Agustin Taverna <agustintaverna.dev@gmail.com>
2024-04-26 01:41:05 -0300

    Refactoriza rutas por grupos de controlador

--- routes/api.php
+++ routes/api.php
@@ -2,17 +2,35 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\ResetPasswordController;
+use App\Http\Controllers\TokenController;
+use Laravel\Sanctum\Sanctum;
 
-Route::post('/register', [AuthController::class, 'register']);
-Route::post('/login', [AuthController::class, 'login']);
-Route::post('/logout', [AuthController::class, 'logout']);
-
+Route::controller(AuthController::class)->group(function (){
+    Route::post('/register', 'register');
+    Route::post('/login', 'login');
+    Route::post('/logout', 'logout')->middleware(['auth:sanctum']);
+});
 
 Route::controller(ResetPasswordController::class)->group(function (){
     Route::post('/forgot-password', 'sendLinkResetPassword')->name('user.forgot-password');
     Route::post('/new-password', 'resetPassword')->name('user.reset-password-from-mail');
     Route::post('/reset-password', 'resetPassword')->name('user.reset-password')->middleware(['auth:sanctum']);
 });
+
+Route::controller(PostController::class)->middleware(['auth:sanctum'])->group(function (){
+    Route::post('/create-post', 'create');
+    Route::post('/update-post', 'update');
+});
+
+Route::controller(ProfileController::class)->middleware(['auth:sanctum'])->group(function (){
+    Route::get('/get-profile', 'getProfile');
+    Route::put('/update-profile', 'updateProfile');
+});
+
+
+
 
 
