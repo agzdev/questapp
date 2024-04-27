6c030d1bed31522c5a6d8c1955d7831018fc7f05
Agustin Taverna <agustintaverna.dev@gmail.com>
2024-04-26 21:55:20 -0300

    Refactoriza metodo para actualizar los datos del perfil de usuario

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -6,32 +6,29 @@ use App\Models\User;
 use Illuminate\Http\Request;
 use App\Http\Requests\UpdateProfileRequest;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 
 class ProfileController extends Controller
 {
-    public function getProfile()
+    public function getProfile():JsonResponse
     {
         $user = auth()->user();
 
         return response()->json($user, Response::HTTP_OK);
     }
 
-    public function updateProfile(UpdateProfileRequest $request)
+    public function updateProfile(UpdateProfileRequest $request, User $user):JsonResponse
     {
-        try {
-            $user = User::findOrFail(auth()->user()->id);
-        } catch (ModelNotFoundException $e) {
-            return response()->json('User not logged in.', Response::HTTP_UNAUTHORIZED);
+        if(!$user){
+            return response()->json('User not found.', Response::HTTP_NOT_FOUND);
         }
-
         $user->update([
-            'full_name' => isset($request->full_name) ? $request->full_name : $user->full_name,
-            'username' => isset($request->username) ? $request->username : $user->username,
-            'email' => isset($request->email) ? $request->email : $user->email,
-            'avatar' => isset($request->avatar) ? $request->avatar : $user->avatar
+            'full_name' => $request->has('full_name') ? $request->full_name : $user->full_name,
+            'username' => $request->has('username') ? $request->username : $user->username,
+            'email' => $request->has('email') ? $request->email : $user->email,
+            'avatar' => $request->has('avatar') ? $request->avatar : $user->avatar
         ]);
-
         return response()->json(['Profile updated successfully', $user], Response::HTTP_OK);
     }
 
